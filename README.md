3e308d42b25e9e6cf93e122854e71e5b30e23546
Alessandro Borraccino <alessandro.borraccino@sparkling18.com>
2019-05-20 16:15:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pattern-recognition-feature-points
---
+++ src/test/java/com/aborraccino/patternrecognition/featurepoints/featurepoints/FeaturePointsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aborraccino.patternrecognition.featurepoints.featurepoints;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeaturePointsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
